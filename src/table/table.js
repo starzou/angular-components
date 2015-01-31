6e5fb56872822bb97a44e9674431dc8395fcbe0b
starzou <starzou@126.com>
2015-01-31 17:29:19 +0800

    table 控件开发

--- src/table/table.js
+++ src/table/table.js
@@ -9,16 +9,54 @@
 
     var tableModule = angular.module('ngComponents.table', []);
 
-    tableModule.directive('uiTable', [function () {
+    tableModule.provider('$table', function () {
+        var defaults = this.defaults = {
+            customClass: '',
+            prefixClass: 'table',
+            prefixEvent: 'table',
+            template   : 'table/table.tpl.html',
+            pager      : {
+                currentPage: 1,
+                pageSize   : 20
+            },
+            sizeList   : [10, 20, 50, 100, 150]
+        };
+
+        this.$get = ['$rootScope', '$$rAF', '$templateRequest', function ($rootScope, $$rAF, $templateRequest) {
+
+            function TableFactory($element, config) {
+                var $table = {}; // table 组件
+                var options = $table.$options = angular.extend({}, defaults, config); // table 配置
+                var scope = $table.$scope = options.scope && options.scope.$new() || $rootScope.$new(); // table组件的 scope
+
+                return $table;
+            }
+
+            return TableFactory;
+        }];
+    });
+
+    tableModule.directive('uiTable', ['$table', function ($table) {
         return {
-            restrict   : 'EA',
-            scope      : true,
-            replace    : true,
-            templateUrl: 'table/table.tpl.html',
-            compile    : function ($element, $attr) {
-                return function ($scope, $element, $attr) {
-                    console.log('uiTable link...');
-                };
+            restrict: 'EA',
+            scope   : true,
+            link    : function postLink($scope, $element, $attr) {
+                console.log('uiTable link...');
+
+                var options = {scope: $scope};
+
+                angular.forEach(['type', 'url'], function (key) {
+                    if (angular.isDefined($attr[key])) {
+                        options[key] = $attr[key];
+                    }
+                });
+
+                var tooltip = $table($element, options);
+
+                $scope.$on('$destroy', function () {
+                    options = null;
+                    tooltip = null;
+                });
             }
         };
     }]);
